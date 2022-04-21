20408bfd8049972d8d018f58dec977d3529dfe0f
amravy <99766520+amravy@users.noreply.github.com>
2022-04-21 09:53:31 -0500

    Figured out how to copy and paste in JS.

---
+++ popup.js
@@ -0,0 +1,9 @@
+// Create a function to copy the text from a textarea to the clipboard
+
+function copyToClipboard() {
+    var copyText = document.getElementById("codeRequest");
+    copyText.select();
+    document.execCommand("copy");
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the text: " + copyText.value);
+}``
